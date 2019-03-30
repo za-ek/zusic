bfdbb0c5df64f77ccc9c760eb3a215a3e26f8e21
Zakhar Telyatnikov <tz@za-ek.ru>
2019-03-30 21:27:57 +0300

    Icons

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,114 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Layout>
+      <div slot="artist-list">
+        Artist list
+        <div v-for="i in artists" :key="i">
+          {{i}} <br>
+        </div>
+      </div>
+      <div slot="album-list">
+        Album list
+        <div v-for="i in albums" :key="i">
+          {{i}} <br>
+        </div>
+      </div>
+      <div slot="track-list">
+        Track list
+        <div v-for="i in tracks" :key="i">
+          {{i}} <br>
+        </div>
+      </div>
+      <div slot="playlist">
+        Playlist
+        <div v-for="i in playlist" :key="i">
+          {{i}} <br>
+        </div>
+      </div>
+      <div slot="play-line" style="width: 100%;height:100%; display: block">
+        <div id="controls">
+          <v-icon name="skip-back" id="play-line-backward"></v-icon>
+          <v-icon name="play-circle" id="play-line-play"></v-icon>
+          <v-icon name="skip-forward" id="play-line-forward"></v-icon>
+        </div>
+      </div>
+    </Layout>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Layout from './layouts/Main'
 
 export default {
   name: 'app',
+  data () {
+    return {
+      artists: [],
+      albums: [],
+      tracks: [],
+      playlist: []
+    }
+  },
+  created () {
+    for (let i = 0; i < 40; i++) {
+      this.artists.push(Math.random().toString(36).substring(7))
+      this.albums.push(Math.random().toString(36).substring(7))
+      this.tracks.push(Math.random().toString(36).substring(7))
+      this.playlist.push(Math.random().toString(36).substring(7))
+    }
+  },
   components: {
-    HelloWorld
+    Layout
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  margin:0;
+  padding:0;
+  height:100vh;
+}
+
+#controls {
+  margin: 0 auto;
+  display: block;
+  height:100%;
+  width: 120px;
+}
+
+#play-line-backward {
+  display: block;
+  float:left;
+  margin-top:8px;
+  height:25px;
+  background-color: purple;
+  border-radius: 30px;
+  padding:3px;
+  border:2px solid #ed00ed;
+  margin-right:2px;
+  color: #fff;
+}
+#play-line-play {
+  display: block;
+  float:left;
+  height:34px;
+  margin-top: 5px;
+  background-color: purple;
+  border-radius: 30px;
+  padding:3px;
+  color: #fff;
+}
+#play-line-forward {
+  display: block;
+  float:left;
+  margin-top:8px;
+  margin-left:2px;
+  height:25px;
+  background-color: purple;
+  border-radius: 30px;
+  padding:3px;
+  border:2px solid #ed00ed;
+  color: #fff;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import feather from 'vue-icon'
+
+Vue.use(feather, 'v-icon')
 
 Vue.config.productionTip = false
 
